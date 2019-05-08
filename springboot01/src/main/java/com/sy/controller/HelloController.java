42af7285a2ae53edc9b03109c02817118fc938b9
sunyubk <1870971964@qq.com>
2019-05-08 14:36:00 +0800

    test

--- springboot01/src/main/java/com/sy/controller/HelloController.java
+++ springboot01/src/main/java/com/sy/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by SY on 2019/5/8.
  */
-//@ResponseBody //这个类的所有方法返回的数据直接写给浏览器（如果是对象，转换成json）
+//@ResponseBody //s 这个类的所有方法返回的数据直接写给浏览器（如果是对象，转换成json）
 //@Controller
 @RestController
 public class HelloController {
