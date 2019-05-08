7daceeefe4a9b623b02d29e8532bca1f236825cc
sunyubk <1870971964@qq.com>
2019-05-08 14:34:15 +0800

    springboot01

---
+++ springboot01/src/main/java/com/sy/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.sy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by SY on 2019/5/8.
+ */
+//@ResponseBody //这个类的所有方法返回的数据直接写给浏览器（如果是对象，转换成json）
+//@Controller
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}
