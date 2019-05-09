61e3cb6f6c325152131bf6748e0f95419494d774
sunyubk <1870971964@qq.com>
2019-05-09 11:34:54 +0800

    springboot配置文件的的各种应用

--- spring-boot-02-config/src/main/java/com/sy/bean/Person.java
+++ spring-boot-02-config/src/main/java/com/sy/bean/Person.java
@@ -13,14 +13,16 @@ import java.util.Map;
  * Created by SY on 2019/5/8.
  */
 
+
+//@PropertySource(value = {"classpath:person.properties"})  //导入外部配置文件
 /**
  * 将配置文件中配置的每个属性的值，映射到这个组件中
  * @ConfigurationProperties:告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
  *      prefix = "persion" :配置文件中那个下面的属性进行一一映射(默认从全局配置文件中获取)
- * 只有这个组件是容器中的组件，才能使用容器提供的功能
+ * 只有这个组件是容器中的组件，才能使用容器提供的@ConfigurationProperties功能
  */
-//@PropertySource(value = {"classpath:person.properties"})  //导入外部配置文件
 @Component //将下面这行加载到容器中
+
 @ConfigurationProperties(prefix = "person")
 @Validated  //数据校验
 public class Person {
