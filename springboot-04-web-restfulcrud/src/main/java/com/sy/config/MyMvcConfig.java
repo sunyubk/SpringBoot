081178fb9a8cf3cf51af28d8014421563b1dac1c
sunyubk <1870971964@qq.com>
2019-06-06 12:51:19 +0800

    SpringBoot使用Thymeleaf和SpringMVC的配置

---
+++ springboot-04-web-restfulcrud/src/main/java/com/sy/config/MyMvcConfig.java
@@ -0,0 +1,29 @@
+package com.sy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by SY on 2019/6/6.
+ */
+
+/**
+ * 扩展配置既保留了自动配置也能扩展配置
+ * 可以使用WebMvcConfigurerAdapter来扩展Spring Mvc的功能
+ * 就不用再为springmvc配置xml文件了
+ *
+ * @EnableWebMvc:全面接管SpringMVC,SpringBoot对SpringMVC的自动配置不要了,所有SpringMVC的自动配置全部失效
+ */
+//@EnableWebMvc
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+    //ctrl+o  打开WebMvcConfigurerAdapter可以重写的方法
+    //addViewControllers：视图映射
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //super.addViewControllers(registry);
+        registry.addViewController("/say").setViewName("/success");
+    }
+}
---
+++ springboot-04-web-restfulcrud/src/main/java/com/sy/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.sy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * Created by SY on 2019/6/4.
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello word!";
+    }
+
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("name","<h1>张三</h1>");
+        map.put("users", Arrays.asList("a","b","c"));
+        return "success";
+    }
+}
